692ce1cdaac267a5f541dfd7c98fbf35f58989af
shm-wtag <maruf.shafique@welldev.io>
2025-04-29 02:16:13 +0600

    add keymaps for moving cursor between window

--- modules/neovim/keymaps.lua
+++ modules/neovim/keymaps.lua
@@ -3,3 +3,9 @@ vim.keymap.set("n", "<leader>lf", function()
 end, { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>e", '<cmd>Neotree toggle<cr>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
