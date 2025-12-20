fe83fa6ec9ba473605ba1fbc637674121ee20ed7
shm-wtag <maruf.shafique@welldev.io>
2025-12-20 23:54:07 +0600

    update font for ghostty

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -4,6 +4,7 @@
 		enable = true;
     enableZshIntegration = true;
     settings = {
+			font-family = "DroidSansM Nerd Font Mono";
       font-size = 16;
       shell-integration = "zsh";
 			window-decoration = "none";
