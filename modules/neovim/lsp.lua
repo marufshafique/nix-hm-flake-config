9d3dd7c088ec5c40b6e0c2265260bcfa31d6b07b
Maruf Shafique <maruf.shafique@welldev.io>
2026-05-17 18:19:24 +0600

    add on attach function for goto defination

--- modules/neovim/lsp.lua
+++ modules/neovim/lsp.lua
@@ -1,31 +1,58 @@
 -- Setup completion
-require('blink.cmp').setup({
+require("blink.cmp").setup({
   keymap = {
-    preset = 'default',
-    ['<C-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+    preset = "default",
+    ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
   },
   sources = {
-    default = { 'lsp', 'path', 'buffer' },
+    default = { "lsp", "path", "buffer" },
   },
 })
 
-vim.lsp.config('*', {
-  capabilities = require('blink.cmp').get_lsp_capabilities({}, true),
+local on_attach = function(_, bufnr)
+  local bufmap = function(keys, func)
+    vim.keymap.set("n", keys, func, { buffer = bufnr })
+  end
+
+  bufmap("<leader>r", vim.lsp.buf.rename)
+  bufmap("<leader>a", vim.lsp.buf.code_action)
+
+  bufmap("gd", vim.lsp.buf.definition)
+  bufmap("gD", vim.lsp.buf.declaration)
+  bufmap("gI", vim.lsp.buf.implementation)
+  bufmap("<leader>D", vim.lsp.buf.type_definition)
+
+  bufmap("gr", require("telescope.builtin").lsp_references)
+  bufmap("<leader>s", require("telescope.builtin").lsp_document_symbols)
+  bufmap("<leader>S", require("telescope.builtin").lsp_dynamic_workspace_symbols)
+
+  bufmap("<leader>k", vim.lsp.buf.hover)
+
+  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+    vim.lsp.buf.format()
+  end, {})
+end
+
+vim.lsp.config("*", {
+  on_attach = on_attach,
+  capabilities = require("blink.cmp").get_lsp_capabilities({}, true),
 })
 
 vim.lsp.enable({
-  'lua_ls',
-  'nil',
-  'nixd',
-  'gopls',
-  'ts_ls',
-  'vue',
+  "lua_ls",
+  "nil",
+  "nixd",
+  "gopls",
+  "ts_ls",
+  "vue",
+  "tailwindcss",
+  "eslint",
 })
 
-vim.lsp.config('gopls', {
-  cmd = { 'gopls' },
-  filetypes = { 'go', 'gomod' },
-  root_markers = { 'go.work', 'go.mod', '.git' },
+vim.lsp.config("gopls", {
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod" },
+  root_markers = { "go.work", "go.mod", ".git" },
   settings = {
     gopls = {
       analyses = {
@@ -38,7 +65,7 @@ vim.lsp.config('gopls', {
 })
 
 vim.lsp.config("ts_ls", {
-  cmd = { 'typescript-language-server', '--stdio' },
+  cmd = { "typescript-language-server", "--stdio" },
   init_options = {
     plugins = {
       {
@@ -55,7 +82,7 @@ vim.lsp.config("ts_ls", {
   },
 })
 
-vim.lsp.config('lua_ls', {
+vim.lsp.config("lua_ls", {
   cmd = { "lua-language-server" },
   filetypes = { "lua" },
   settings = {
@@ -72,40 +99,53 @@ vim.lsp.config('lua_ls', {
 })
 
 -- ESLint Server
-vim.lsp.config('eslint', {
+vim.lsp.config("eslint", {
   filetypes = {
-    'javascript',
-    'javascriptreact',
-    'typescript',
-    'typescriptreact',
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
   },
-  root_dir = vim.fs.root(0,
-    { '.eslintrc.js', '.eslintrc.cjs', '.eslintrc.yaml', '.eslintrc.json', 'eslint.config.js', 'package.json', '.git' }),
+  root_dir = vim.fs.root(0, {
+    ".eslintrc.js",
+    ".eslintrc.cjs",
+    ".eslintrc.yaml",
+    ".eslintrc.json",
+    "eslint.config.js",
+    "package.json",
+    ".git",
+  }),
   settings = {
     codeActionOnSave = {
       enable = true,
-      mode = 'all',
+      mode = "all",
     },
-    run = 'onType',
-    validate = 'on',
+    run = "onType",
+    validate = "on",
   },
 })
 
 -- Tailwind CSS Server
-vim.lsp.config('tailwindcss', {
+vim.lsp.config("tailwindcss", {
   filetypes = {
-    'html',
-    'css',
-    'scss',
-    'javascript',
-    'javascriptreact',
-    'typescript',
-    'typescriptreact',
+    "html",
+    "css",
+    "scss",
+    "jsx",
+    "tsx",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
   },
-  root_dir = vim.fs.root(0, { 'tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js', 'package.json', '.git' }),
+  cmd = { "tailwindcss-language-server", "--stdio" },
+  root_dir = vim.fs.root(
+    0,
+    { "tailwind.config.js", "tailwind.config.ts", "postcss.config.js", "package.json", ".git" }
+  ),
   settings = {
     tailwindCSS = {
-      classAttributes = { 'class', 'className', 'classList', 'ngClass' },
+      classAttributes = { "class", "className", "classList", "ngClass" },
     },
   },
 })
