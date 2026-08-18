fd92bbf9cf56b58b94c9fb298f101b1cecd2f16e
shm-wtag <maruf.shafique@welldev.io>
2026-08-18 23:37:42 +0600

    remove android studio

--- home.nix
+++ home.nix
@@ -22,9 +22,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    android-studio
-    android-tools
-
     dig
     lazygit
     fastfetch
